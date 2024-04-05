7b1788cf5c6fb54ced19c0991ff36b7b05b614be
RiturajAgrahari <agr.rituraj@gmail.com>
2024-04-05 11:41:37 +0530

    first view is bit changed and responsiveness added

--- src/components/home_page/firstViewPage/thumbnailvideo.tsx
+++ src/components/home_page/firstViewPage/thumbnailvideo.tsx
@@ -15,8 +15,17 @@ const MainVideo = () => {
         <div className="main_video" id="home">
             <video muted autoPlay id="main_video" loop src={video}></video>
             <div className="video_slogan">
-                <img style={{width:'350px'}} src={favicon}></img>
-                <a className='pre_register' href='#preRegister' onClick={(e) => handleScroll('preRegister', e)}>Pre-Register Now</a>
+                <div className="video_slogan_super">SURVIVE IN TEOT</div>
+                <div className="video_slogan_sub">in this dark world</div>
+                <div className="buttons">
+                    <div className="row1">
+                        <a className='pre_register' href='#preRegister' onClick={(e) => handleScroll('preRegister', e)}>Pre-Register Now</a>
+                        <a className='pre_register' href='#download' onClick={(e) => handleScroll('download', e)}>Download Now</a>
+                    </div>
+                    <div className="row2">
+                        <a className='pre_register' href='#download' onClick={(e) => handleScroll('download', e)}>Watch Trailer Now                        <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="35" viewBox="0 -960 960 960" width="35"><path d="M170-228q-38-44-61-98T80-440h82q6 44 22 83.5t42 72.5l-56 56ZM80-520q8-60 30-114t60-98l56 56q-26 33-42 72.5T162-520H80ZM438-82q-60-6-113.5-29T226-170l56-58q35 26 73.5 43t82.5 23v80ZM284-732l-58-58q45-36 98.5-59T440-878v80q-45 6-84 23t-72 43Zm96 432v-360l280 180-280 180ZM520-82v-80q121-17 200.5-107T800-480q0-121-79.5-211T520-798v-80q154 17 257 130t103 268q0 155-103 268T520-82Z"/></svg></a>
+                    </div>
+                </div>
             </div>
         </div> 
     )
--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFoundPage from './components/notFoundPage/404';
 import Pp from './components/privacyPolicy/pp';
 import Support from './components/support/support';
-import Admin from './admin';
-import SliderOperation from './components/admin/sliderOperations';
-import DashboardSidebar from './components/admin/dashboardSidebar';
-import Dashboard from './components/admin/main';
-import DevblogOperations from './components/admin/devblogsEditing';
 import NewsDetail from './components/home_page/newsDetail/newsdetail';
 import NewsPage from './components/home_page/newsPage/news';
 
@@ -29,10 +24,6 @@ root.render(
         <Route path="/Terms Of Conditions" element={<Tos />} />
         <Route path="/Privacy Policy" element={<Pp />} />
         <Route path="/Support" element={<Support />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/slider" element={<div><DashboardSidebar /><SliderOperation /></div>} />
-        <Route path="/admin/devblog" element={<div><DashboardSidebar /><DevblogOperations /></div>} />
-        <Route path="/admin/dashboard" element={<div><DashboardSidebar /><Dashboard /></div>} />
         <Route path="/news/:id" element={<div><NewsDetail /></div>} />
         <Route path="/news/" element={<div><NewsPage /></div>} />
         {/* Add a catch-all route for 404 */}
