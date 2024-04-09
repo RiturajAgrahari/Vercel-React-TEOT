74605e4f55e2700f3214ce25d821160a52b1f118
RiturajAgrahari <agr.rituraj@gmail.com>
2024-04-09 22:48:44 +0530

    try to precisely see the game introduction section

--- src/components/home_page/description/fix.tsx
+++ src/components/home_page/description/fix.tsx
@@ -4,14 +4,16 @@ import './fix.css'
 
 const Fix = () => {
     return (
-        <div className="random_page">
-            <h1 className="introduction_heading hidden">INTRODUCTION</h1>
-            <p className="introduction_content hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure mollitia repudiandae commodi tempora veritatis? Possimus sint esse tenetur! Error sed consequatur hic aliquid excepturi enim labore eaque, necessitatibus ex?
-            Sequi optio reprehenderit voluptatem, aspernatur ipsam error ducimus! Eum dolor repellat repellendus illo excepturi aperiam atque aspernatur iure autem fugiat, alias nemo quibusdam vero, pariatur sequi nobis dolorum! Ab?
-            Quidem, aspernatur! Qui maxime dolorem aliquam temporibus eveniet voluptate illum explicabo asperiores quod eligendi mollitia provident, vel nam cumque repellendus. Aliquid corrupti sunt, assumenda facilis aliquam recusandae ullam dignissimos.
-            Earum itaque omnis error voluptate. Officiis asperiores optio magni velit incidunt, nostrum quisquam reprehenderit dolores tempore excepturi quia suscipit esse animi consequuntur beatae perspiciatis nesciunt praesentium autem sequi odit!</p>
+        <div className="game_introduction">
+            <div className="random_page">
+                <h1 className="introduction_heading hidden">INTRODUCTION</h1>
+                <p className="introduction_content hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure mollitia repudiandae commodi tempora veritatis? Possimus sint esse tenetur! Error sed consequatur hic aliquid excepturi enim labore eaque, necessitatibus ex?
+                Sequi optio reprehenderit voluptatem, aspernatur ipsam error ducimus! Eum dolor repellat repellendus illo excepturi aperiam atque aspernatur iure autem fugiat, alias nemo quibusdam vero, pariatur sequi nobis dolorum! Ab?
+                Quidem, aspernatur! Qui maxime dolorem aliquam temporibus eveniet voluptate illum explicabo asperiores quod eligendi mollitia provident, vel nam cumque repellendus. Aliquid corrupti sunt, assumenda facilis aliquam recusandae ullam dignissimos.
+                Earum itaque omnis error voluptate. Officiis asperiores optio magni velit incidunt, nostrum quisquam reprehenderit dolores tempore excepturi quia suscipit esse animi consequuntur beatae perspiciatis nesciunt praesentium autem sequi odit!</p>
+            </div>
         </div>
-    )
+    )   
 }
 
 export default Fix
